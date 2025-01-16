51da0b4473bbc054010562febcd41ab2bd0532b9
Andres F. Bonilla Q. <mailofbonilla@gmail.com>
2025-01-16 18:01:24 -0500

    feat: :bricks: add eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default tseslint.config(
-   { ignores: ['dist'] },
+   { ignores: ['dist', 'node_modules'] },
    {
       extends: [js.configs.recommended, ...tseslint.configs.recommended],
       files: ['**/*.{ts,tsx}'],
@@ -19,7 +19,11 @@ export default tseslint.config(
       },
       rules: {
          ...reactHooks.configs.recommended.rules,
-         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+         'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
+         '@typescript-eslint/no-explicit-any': 'error',
+         '@typescript-eslint/no-unused-vars': 'error',
+         'react-hooks/exhaustive-deps': 'off',
+         'react-hooks/rules-of-hooks': 'off'
       }
    }
 );
